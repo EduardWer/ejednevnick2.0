e00bbb1457dc481b0f2cbd957c74553cd93d2226
EduardWer <eduard.unanov66@mail.ru>
2024-02-28 18:10:57 +0000

    last

--- ejednevnick/Class1.cs
+++ ejednevnick/Class1.cs
@@ -7,24 +7,21 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 namespace ejednevnick
-{
-    internal class Class1
+{   
+    internal class data_pro
     {
-        public string path = "json";
-       
-        public class Note
-        {
-            public string Title { get; set; }
-            public string Description { get; set; }
-            public DateTime DueDate { get; set; }
-        }
-
-        
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
 
     }
-
-    static class Serializasial
-    {
+    
+    
+    
+   // 
+    public class Serializasial
+    {   
+        public string path = "C:\\Users\\Eduard\\source\\repos\\ejednevnick\\ejednevnick\\json\\file.json";
         public static void Serialisation<tipe>(tipe data, string path)
         {
             string json = JsonConvert.SerializeObject(data);
@@ -39,3 +36,4 @@ namespace ejednevnick
             return Data; // возвращаем данные 
         }
     }
+}
--- ejednevnick/MainWindow.xaml.cs
+++ ejednevnick/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using static ejednevnick.Class1;
+
 
 namespace ejednevnick
 {
@@ -25,15 +25,60 @@ namespace ejednevnick
         public MainWindow()
         {
             InitializeComponent();
+            dATE_TRANS.SelectedDate = DateTime.Now;
+            
+            Serializasial serializasial = new Serializasial();
+            var info =  Serializasial.DeSerialization<List<data_pro>>(serializasial.path);
+
+            
+            for (int i = 0; i <= info.Count; i++) {
+                //var data_string = info[i].DueDate.Date.ToString();
+                if (info == null)
+                {
+                    return;
+                }
+                
+                
+            }
+
         }
 
 
 
 
         private void sAVE_bUTTON_Click(object sender, RoutedEventArgs e)
-        {
-            Button button = (Button)sender;
-            Zametka.Content ="fsdfsdfsdfsfsdfsdf";
+        {   
+            Serializasial serial =  new Serializasial();
+            data_pro data_Pro = new data_pro();
+           
+            
+
+            List<data_pro> notes = new List<data_pro>();
+            data_pro data_Pro1 = new data_pro();
+            List<data_pro> data_Pros = Serializasial.DeSerialization<List<data_pro>>(serial.path);
+
+            data_Pro1.Title = title_text.Text;
+            data_Pro1.Description = description_text.Text;
+            data_Pro1.DueDate = Convert.ToDateTime( dATE_TRANS.SelectedDate);
+
+            data_Pros.Add(data_Pro1);
+            
+            Serializasial.Serialisation(data: data_Pros, serial.path );
+
+             
+            if (Zametka.Text == null)
+            {
+                Zametka.Text =  "Название   " + data_Pro1.Title + "   Описание  " + data_Pro1.Description+ "   Дата создания  "+ data_Pro1.DueDate;
+            }
+            else
+            {
+                Zametka.Text = Zametka.Text +"\n" + "Название   " + data_Pro1.Title + "   Описание  " + data_Pro1.Description + "   Дата создания  " + data_Pro1.DueDate;
+            }
+            
+
+
+
+            
 
            
             
@@ -44,7 +89,9 @@ namespace ejednevnick
 
         }
 
-
-        
+        private void dATE_TRANS_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+           
+        }
     }
 }
